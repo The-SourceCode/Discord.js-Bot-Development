2799118a5ad7d477e43f93c15f82a7b4e93806db
{TheSourceCode} <The-SourceCode@users.noreply.github.com>
2018-10-23 12:11:09 -0500

    added a check to make sure warns exists

--- episode_8_code/commands/warnlevel.js
+++ episode_8_code/commands/warnlevel.js
@@ -5,6 +5,10 @@ let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
 
 module.exports.run = async (bot, message, args) => {
 
+  if(!warns[wUser.id]) warns[wUser.id] = {
+    warns: 0
+  };
+  
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You can't do that.");
   let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
   if(!wUser) return message.reply("Couldn't find them yo");
