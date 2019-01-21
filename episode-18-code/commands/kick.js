a161c38acab9f5bfb8fadcfd4701590a32b190d8
Nef <33611774+Neeef@users.noreply.github.com>
2019-01-21 15:03:24 -0500

    Update entire episode 18 most recent major code.
    Hopefully I fixed all the deprecation errors, made things more efficient, and just overall improved the code.

--- episode-18-code/commands/kick.js
+++ episode-18-code/commands/kick.js
@@ -3,32 +3,29 @@ const errors = require("../utils/errors.js");
 
 module.exports.run = async (bot, message, args) => {
 
-    if(!message.member.hasPermission("KICK_MEMBERS")) return errors.noPerms(message, "KICK_MEMBERS");
-    if(args[0] == "help"){
-      message.reply("Usage: !kick <user> <reason>");
-      return;
-    }
-    let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!kUser) return errors.cantfindUser(message.channel);
+    if (!message.member.hasPermission("KICK_MEMBERS")) return errors.noPerms(message, "KICK_MEMBERS");
+    if (args[0] === "help") return message.reply("Usage: <prefix>kick @user reason");
+    const kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if (!kUser) return errors.cantfindUser(message.channel);
     let kReason = args.join(" ").slice(22);
-    if(kUser.hasPermission("MANAGE_MESSAGES")) return errors.equalPerms(message, kUser, "MANAGE_MESSAGES");
+    if (kUser.hasPermission("MANAGE_MESSAGES")) return errors.equalPerms(message, kUser, "MANAGE_MESSAGES");
+    if (!kReason) kReason = "Not provided.";
+    const kickEmbed = new Discord.RichEmbed()
+        .setDescription("~Kick~")
+        .setColor("#e56b00")
+        .addField("Kicked User", `${kUser} with ID ${kUser.id}`)
+        .addField("Kicked By", `<@${message.author.id}> with ID ${message.author.id}`)
+        .addField("Kicked In", message.channel)
+        .addField("Tiime", message.createdAt)
+        .addField("Reason", kReason);
 
-    let kickEmbed = new Discord.RichEmbed()
-    .setDescription("~Kick~")
-    .setColor("#e56b00")
-    .addField("Kicked User", `${kUser} with ID ${kUser.id}`)
-    .addField("Kicked By", `<@${message.author.id}> with ID ${message.author.id}`)
-    .addField("Kicked In", message.channel)
-    .addField("Tiime", message.createdAt)
-    .addField("Reason", kReason);
-
-    let kickChannel = message.guild.channels.find(`name`, "incidents");
-    if(!kickChannel) return message.channel.send("Can't find incidents channel.");
+    const kickChannel = message.guild.channels.find(`name`, "incidents");
+    if (!kickChannel) return message.channel.send("Can't find incidents channel.");
 
     message.guild.member(kUser).kick(kReason);
     kickChannel.send(kickEmbed);
-}
+};
 
 module.exports.help = {
   name:"kick"
-}
+};
