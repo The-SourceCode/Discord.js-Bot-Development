27ad8bdf7a8472d1ea3bd4d0d62a6e3fdc2b0811
Stan <stanbarkmeijer@hotmail.com>
2019-09-04 19:58:13 +0200

    EP_04

---
+++ Ep_04/functions.js
@@ -0,0 +1,26 @@
+module.exports = {
+    getMember: function(message, toFind = '') {
+        toFind = toFind.toLowerCase();
+
+        let target = message.guild.members.get(toFind);
+        
+        if (!target && message.mentions.members)
+            target = message.mentions.members.first();
+
+        if (!target && toFind) {
+            target = message.guild.members.find(member => {
+                return member.displayName.toLowerCase().includes(toFind) ||
+                member.user.tag.toLowerCase().includes(toFind)
+            });
+        }
+            
+        if (!target) 
+            target = message.member;
+            
+        return target;
+    },
+
+    formatDate: function(date) {
+        return new Intl.DateTimeFormat('en-US').format(date)
+    }
+}
