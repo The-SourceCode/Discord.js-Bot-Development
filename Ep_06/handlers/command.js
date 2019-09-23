39d01d2bba13d6cee3b7c1436a81056f44d4f529
Stan <31732012+StanBarkmeijer@users.noreply.github.com>
2019-09-23 22:57:40 +0200

    Update command.js

--- Ep_06/handlers/command.js
+++ Ep_06/handlers/command.js
@@ -1,34 +1,3 @@
-// const { readdirSync } = require("fs");
-
-// module.exports = (client) => {
-//     readdirSync("./commands/").forEach(dir => {
-//         const commands = readdirSync(`./commands/${dir}/`).filter(file => file.endsWith(".js"));
-    
-//         for (let file of commands) {
-//             let pull = require(`../commands/${dir}/${file}`);
-
-//             if (typeof(pull.name) === "string") {
-//                 client.commands.set(pull.name, pull);
-//             } else if (Array.isArray(pull.name) && pull.name.length > 1) {
-//                 if (pull.name.length > 1) {
-//                     pull.aliases = pull.name.slice(1);
-//                     pull.name = pull.name[0]
-    
-//                     client.commands.set(pull.name, pull);
-//                 } else {
-//                     client.commands.set(pull.name[0], pull);
-//                 } 
-//             }
-//         }
-//     });
-// }
-
-// let command = client.commands.get(cmd);;
-//     if (!command) command = client.commands.find(c => c.name.includes(cmd));
-
-//     if (command) 
-//         command.run(client, message, args);
-
 const { readdirSync } = require("fs");
 
 const ascii = require("ascii-table");
