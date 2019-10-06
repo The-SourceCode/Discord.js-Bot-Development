937099e45faaaa7e8074770bb2a60e785433f2db
Jabu2 <31703017+Jabu2@users.noreply.github.com>
2019-10-06 22:51:47 +0200

    Update index.js
    Latency is NaN, but with createdAt it works

--- Ep_02/index.js
+++ Ep_02/index.js
@@ -45,7 +45,7 @@ client.on("message", async message => {
         const msg = await message.channel.send(`🏓 Pinging....`);
 
         // Edit the message
-        msg.edit(`🏓 Pong!\nLatency is ${Math.floor(msg.createdTimestap - message.createdTimestap)}ms\nAPI Latency is ${Math.round(client.ping)}ms`);
+        msg.edit(`🏓 Pong!\nLatency is ${Math.floor(msg.createdAt - message.createdAt)}ms\nAPI Latency is ${Math.round(client.ping)}ms`);
     }
 
     if (cmd === "say") {
