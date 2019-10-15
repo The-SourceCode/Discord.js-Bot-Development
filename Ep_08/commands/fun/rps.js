1d8368e47a15ef2fcd3815793c92447970a42058
Stan <stanbarkmeijer@hotmail.com>
2019-10-15 12:50:00 +0200

    Ep_08

---
+++ Ep_08/commands/fun/rps.js
@@ -0,0 +1,48 @@
+const { RichEmbed } = require("discord.js");
+const { promptMessage } = require("../../functions.js");
+
+const chooseArr = ["🗻", "📰", "✂"];
+
+module.exports = {
+    name: "rps",
+    category: "fun",
+    description: "Rock Paper Scissors game. React to one of the emojis to play the game.",
+    usage: "rps",
+    run: async (client, message, args) => {
+        const embed = new RichEmbed()
+            .setColor("#ffffff")
+            .setFooter(message.guild.me.displayName, client.user.displayAvatarURL)
+            .setDescription("Add a reaction to one of these emojis to play the game!")
+            .setTimestamp();
+
+        const m = await message.channel.send(embed);
+        // Wait for a reaction to be added
+        const reacted = await promptMessage(m, message.author, 30, chooseArr);
+
+        // Get a random emoji from the array
+        const botChoice = chooseArr[Math.floor(Math.random() * chooseArr.length)];
+
+        // Check if it's a win/tie/loss
+        const result = await getResult(reacted, botChoice);
+        // Clear the reactions
+        await m.clearReactions();
+
+        embed
+            .setDescription("")
+            .addField(result, `${reacted} vs ${botChoice}`);
+
+        m.edit(embed);
+
+        function getResult(me, clientChosen) {
+            if ((me === "🗻" && clientChosen === "✂") ||
+                (me === "📰" && clientChosen === "🗻") ||
+                (me === "✂" && clientChosen === "📰")) {
+                    return "You won!";
+            } else if (me === clientChosen) {
+                return "It's a tie!";
+            } else {
+                return "You lost!";
+            }
+        }
+    }
+}
