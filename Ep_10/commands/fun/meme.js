84515adc1ab67d5ab725e8686ebb1b625e06d214
Stan <stanbarkmeijer@hotmail.com>
2019-10-28 13:51:39 +0100

    Forgot to upload again

---
+++ Ep_10/commands/fun/meme.js
@@ -0,0 +1,25 @@
+const { RichEmbed } = require("discord.js");
+const randomPuppy = require("random-puppy");
+
+module.exports = {
+    name: "meme",
+    category: "fun",
+    description: "Sends an epic meme",
+    run: async (client, message, args) => {
+        // In this array, 
+        // you can put the subreddits you want to grab memes from
+        const subReddits = ["dankmeme", "meme", "me_irl"];
+        // Grab a random property from the array
+        const random = subReddits[Math.floor(Math.random() * subReddits.length)];
+
+        // Get a random image from the subreddit page
+        const img = await randomPuppy(random);
+        const embed = new RichEmbed()
+            .setColor("RANDOM")
+            .setImage(img)
+            .setTitle(`From /r/${random}`)
+            .setURL(`https://reddit.com/r/${random}`);
+
+        message.channel.send(embed);
+    }
+}
---
+++ Ep_10/commands/fun/rps.js
@@ -0,0 +1,44 @@
+const { RichEmbed } = require("discord.js");
+const { promptMessage } = require("../../functions.js");
+
+const chooseArr = ["🗻", "📰", "✂"];
+
+module.exports = {
+    name: "rps",
+    category: "fun",
+    description: "Rock Paper Scissors game. React to one of the emojis to play the game.",
+    usage: "rps",
+    run: async (client, message, args) => {
+        const embed = new RichEmbed()
+            .setColor("#ffffff")
+            .setFooter(message.guild.me.displayName, client.user.displayAvatarURL)
+            .setDescription("Add a reaction to one of these emojis to play the game!")
+            .setTimestamp();
+
+        const m = await message.channel.send(embed);
+        const reacted = await promptMessage(m, message.author, 30, chooseArr);
+
+        const botChoice = chooseArr[Math.floor(Math.random() * chooseArr.length)];
+
+        const result = await getResult(reacted, botChoice);
+        await m.clearReactions();
+
+        embed
+            .setDescription("")
+            .addField(result, `${reacted} vs ${botChoice}`);
+
+        m.edit(embed);
+
+        function getResult(me, clientChosen) {
+            if ((me === "🗻" && clientChosen === "✂") ||
+                (me === "📰" && clientChosen === "🗻") ||
+                (me === "✂" && clientChosen === "📰")) {
+                    return "You won!";
+            } else if (me === clientChosen) {
+                return "It's a tie!";
+            } else {
+                return "You lost!";
+            }
+        }
+    }
+}
