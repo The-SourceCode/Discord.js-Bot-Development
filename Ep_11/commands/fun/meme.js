b8fba84782c8e22684e42ff7e7f00d501ebc77fa
Stan <stanbarkmeijer@hotmail.com>
2019-11-04 10:23:13 +0100

    EP11

---
+++ Ep_11/commands/fun/meme.js
@@ -0,0 +1,21 @@
+const { RichEmbed } = require("discord.js");
+const randomPuppy = require("random-puppy");
+
+module.exports = {
+    name: "meme",
+    category: "fun",
+    description: "Sends an epic meme",
+    run: async (client, message, args) => {
+        const subReddits = ["dankmeme", "meme", "me_irl"];
+        const random = subReddits[Math.floor(Math.random() * subReddits.length)];
+
+        const img = await randomPuppy(random);
+        const embed = new RichEmbed()
+            .setColor("RANDOM")
+            .setImage(img)
+            .setTitle(`From /r/${random}`)
+            .setURL(`https://reddit.com/r/${random}`);
+
+        message.channel.send(embed);
+    }
+}
